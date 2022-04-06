4c0b3f6ced5c10906a98f89819aaf4d9b83b7224
tiagovgomes <tiagovideiragomes@hotmail.com>
2022-04-06 21:28:02 +0800

    merge sort

--- algorithms-visualizer/src/App.js
+++ algorithms-visualizer/src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import SortingVisualizer from './sortingVisualizer/sortingVisualizer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h6>TESTING NEW APP!!</h6>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SortingVisualizer></SortingVisualizer>
     </div>
   );
 }
