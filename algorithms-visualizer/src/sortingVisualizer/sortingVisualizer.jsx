82b42d28417a731f8e518ad72147bc23d10d6943
tiagovgomes <tiagovideiragomes@hotmail.com>
2022-07-09 16:35:49 +0800

    change ui

--- algorithms-visualizer/src/sortingVisualizer/sortingVisualizer.jsx
+++ algorithms-visualizer/src/sortingVisualizer/sortingVisualizer.jsx
@@ -10,13 +10,11 @@ import * as algos from '../sortingAlgorithms/sortingAlgorithms';
 const PRIMARY_COLOR = 'turquoise';
 const SECONDARY_COLOR = 'red';
 
-const ANIMATION_SPEED_MS = 4;
-
-
 
 export default function SortingVisualizer() {
     const [array, setArray] = useState([]);
     const [numberOfBars, setNumberOfBars] = useState(95);
+    const [speed, setSpeed] = useState(50);
 
 
     useEffect(() => {
@@ -32,12 +30,12 @@ export default function SortingVisualizer() {
                 const color = animations[i].action === algos.COMPARE_NUMS_SECONDARY_COLOR ? SECONDARY_COLOR : PRIMARY_COLOR;
                 setTimeout(() => {
                     barOneStyle.backgroundColor = color;
-                }, i * ANIMATION_SPEED_MS);
+                }, Math.floor(i * speed / 5));
             } else {
                 setTimeout(() => {
                     const barOneStyle = arrayBars[animations[i].index].style;
                     barOneStyle.height = `${animations[i].value}px`;
-                }, i * ANIMATION_SPEED_MS);
+                }, Math.floor(i * speed / 5));
             }
         }
     }
@@ -84,14 +82,32 @@ export default function SortingVisualizer() {
 
     return (
         <div className="visualizer-container">
+            <div className="header-container">
+                <div className="title-algo-buttons-container">
+                    <div className="visualizer-title">
+                        sorting visualizer
+                    </div>
+                    <ButtonGroup className="button-group-container" variant="contained" aria-label="outlined primary button group">
+                        <Button onClick={() => resetArray()}>Generate New Array</Button>
+                        <Button onClick={() => sortAlgorithm('Merge')}>Merge Sort</Button>
+                        <Button onClick={() => sortAlgorithm('Quick')}>Quick Sort</Button>
+                        <Button onClick={() => sortAlgorithm('Heap')}>Heap Sort</Button>
+                        <Button onClick={() => sortAlgorithm('Bubble')}>Bubble Sort</Button>
+                    </ButtonGroup>
+                </div>
+                <div className="sliders-container">
+                    <Typography id="input-slider" gutterBottom>
+                        Number of bars
+                    </Typography>
+                    <Slider onChange={handleChange} aria-labelledby="input-slider" valueLabelDisplay="on" defaultValue={100} min={20} max={200} aria-label="Default" />
+                    <Typography id="input-slider" gutterBottom>
+                        Speed
+                    </Typography>
+                    <Slider onChange={(event, value) => { setSpeed(value) }} aria-labelledby="input-slider" valueLabelDisplay="on" defaultValue={50} min={1} max={100} aria-label="Default" />
+                </div>
+            </div>
             <div className="button-and-visualizer-container">
-                <ButtonGroup className="button-group-container" orientation="vertical" variant="contained" aria-label="outlined primary button group">
-                    <Button onClick={() => resetArray()}>Generate New Array</Button>
-                    <Button onClick={() => sortAlgorithm('Merge')}>Merge Sort</Button>
-                    <Button onClick={() => sortAlgorithm('Quick')}>Quick Sort</Button>
-                    <Button onClick={() => sortAlgorithm('Heap')}>Heap Sort</Button>
-                    <Button onClick={() => sortAlgorithm('Bubble')}>Bubble Sort</Button>
-                </ButtonGroup>
+
                 <div className="array-container">
                     {array.map((value, idx) => (
                         <div
@@ -104,12 +120,6 @@ export default function SortingVisualizer() {
                         </div>
                     ))}
                 </div>
-            </div>
-            <div className="slider-container">
-                <Typography id="input-slider" gutterBottom>
-                    Number of bars
-                </Typography>
-                <Slider onChange={handleChange} aria-labelledby="input-slider" valueLabelDisplay="on" defaultValue={95} min={20} max={200} aria-label="Default" />
             </div>
         </div>
 
